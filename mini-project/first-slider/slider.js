9c8feba5baad7d00533a1c99c3f3ec9520671431
Stocil <vashurov02@mail.ru>
2023-08-18 23:57:21 +0300

    slider complite

--- mini-project/first-slider/slider.js
+++ mini-project/first-slider/slider.js
@@ -1,11 +1,23 @@
 const ul = document.body.querySelector("ul");
+const leftButton = document.body.querySelector(".slider__left-button");
+const rightButton = document.body.querySelector(".slider__right-button");
 const sliderMaxWidth = ul.childElementCount * 130;
 
-ul.addEventListener("click", scroll);
+rightButton.addEventListener("click", scrollRight);
 
-function scroll() {
+function scrollRight() {
   ul.scrollTo({
     left: ul.scrollLeft + 390,
+    top: 0,
+    behavior: "smooth",
+  });
+}
+
+leftButton.addEventListener("click", scrollLeft);
+
+function scrollLeft() {
+  ul.scrollTo({
+    left: ul.scrollLeft - 390,
     top: 0,
     behavior: "smooth",
   });
