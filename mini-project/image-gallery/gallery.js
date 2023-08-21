ebd297e4e83bc3392b51f40fdec98158430c0b81
Stocil <vashurov02@mail.ru>
2023-08-22 00:04:23 +0300

    image-gallery update

--- mini-project/image-gallery/gallery.js
+++ mini-project/image-gallery/gallery.js
@@ -8,38 +8,15 @@ thumbs.addEventListener("click", function (event) {
   const image = event.target;
   const imageSrc = image.getAttribute("src");
 
-  image.setAttribute("src", mainImage.getAttribute("src"));
+  // Сделать обмен
+  // image.setAttribute("src", mainImage.getAttribute("src"));
+
   mainImage.setAttribute("src", imageSrc);
+
+  image.style.opacity = "0";
+  mainImage.style.opacity = "0";
+  setTimeout(function () {
+    image.style.opacity = "1";
+    mainImage.style.opacity = "1";
+  }, 300);
 });
-
-// Второе решение
-
-// const inner = document.body.querySelector(".inner");
-
-// inner.addEventListener("click", function (event) {
-//   event.preventDefault();
-//   if (event.target.tagName != "IMG") return;
-
-//   let mainImage = document.body.querySelector(".largeImg");
-//   let image;
-
-//   if (event.target.classList.contains("largeImg")) return;
-
-//   image = event.target;
-//   imageNumber = image.closest("a").getAttribute("title").slice(-1);
-
-//   mainImage.setAttribute("src", image.getAttribute("src"));
-// });
-
-// Изначальное решение
-
-// mainImageNumber = imageNumber;
-
-// mainImageScr =
-//   mainImage.getAttribute("src").split("/")[0] +
-//   "/" +
-//   mainImageNumber +
-//   mainImage.getAttribute("src").split("/")[1].slice(1);
-
-// mainImage.setAttribute("src", mainImageNumber);
-// console.log(mainImageScr);
