ec981598e408871e22bc144e7b78cbb4acf472ea
Stocil <vashurov02@mail.ru>
2023-08-22 02:16:38 +0300

    image-gallery full slider complite

--- mini-project/image-gallery/gallery.js
+++ mini-project/image-gallery/gallery.js
@@ -20,3 +20,37 @@ thumbs.addEventListener("click", function (event) {
     mainImage.style.opacity = "1";
   }, 300);
 });
+
+const slider = document.body.querySelector(".slider");
+const sliderInner = document.body.querySelector(".slider__inner");
+const thumbsLength = document.body.querySelectorAll("li").length;
+const sliderMaxWidth =
+  document.body.querySelector(".thumbs__item").clientWidth * 6 + "px";
+
+const thumbsWidth =
+  document.body.querySelector(".thumbs__item").clientWidth * thumbsLength +
+  "px";
+
+let translateX = 0;
+
+sliderInner.style.width = sliderMaxWidth;
+thumbs.style.width = thumbsWidth;
+
+slider.addEventListener("click", function (event) {
+  if (event.target.tagName != "BUTTON") return;
+
+  if (event.target.classList.contains("slider__button-left")) {
+    // Слайдер не выходит за левую границу
+    if (translateX >= 0) return;
+
+    translateX += 118;
+    thumbs.style.transform = `translateX(${translateX}px)`;
+  } else {
+    // Слайдер не выходит за правую границу. Учитывая нынешнюю прокрутку и размер слайдера
+    if (translateX - +sliderMaxWidth.slice(0, -2) <= -+thumbsWidth.slice(0, -2))
+      return;
+
+    translateX -= 118;
+    thumbs.style.transform = `translateX(${translateX}px)`;
+  }
+});
