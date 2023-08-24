f89572ba6895617b06ea577d9ba1739ed326cd1a
Stocil <vashurov02@mail.ru>
2023-08-24 19:56:14 +0300

    image-gallery update + backup

--- mini-project/image-gallery/gallery.js
+++ mini-project/image-gallery/gallery.js
@@ -33,70 +33,76 @@ topSlider.addEventListener("click", function (event) {
       rightSibling.classList.remove("selected");
       image.closest("li").classList.add("selected");
 
-      topSliderTranslateX += 290;
-      sliderUl.style.transform = `translateX(${topSliderTranslateX}px)`;
+      ScrollLeft(rightSibling);
+
+      // topSliderTranslateX += 290;
+      // sliderUl.style.transform = `translateX(${topSliderTranslateX}px)`;
 
       // Уход за изображение
-      if (rightSibling.nextElementSibling) {
-        rightSibling.nextElementSibling.style.transform = `translate(${-290}px, ${-20}px)`;
-        rightSibling.nextElementSibling.style.zIndex = `-1`;
-        setTimeout(function () {
-          rightSibling.nextElementSibling.style.opacity = `0`;
-        }, 300);
-        setTimeout(function () {
-          rightSibling.nextElementSibling.style.transform = `translateX(${0}px)`;
-        }, 400);
-        setTimeout(function () {
-          rightSibling.nextElementSibling.style.zIndex = `0`;
-          rightSibling.nextElementSibling.style.opacity = `1`;
-        }, 1000);
-      }
+      // if (rightSibling.nextElementSibling) {
+      //   rightSibling.nextElementSibling.style.transform = `translate(${-290}px, ${-20}px)`;
+      //   rightSibling.nextElementSibling.style.zIndex = `-1`;
+      //   setTimeout(function () {
+      //     rightSibling.nextElementSibling.style.opacity = `0`;
+      //   }, 300);
+      //   setTimeout(function () {
+      //     rightSibling.nextElementSibling.style.transform = `translateX(${0}px)`;
+      //   }, 400);
+      //   setTimeout(function () {
+      //     rightSibling.nextElementSibling.style.zIndex = `0`;
+      //     rightSibling.nextElementSibling.style.opacity = `1`;
+      //   }, 1000);
+      // }
     } else {
       // Правая кнопка
       leftSibling.classList.remove("selected");
       image.closest("li").classList.add("selected");
 
-      topSliderTranslateX -= 290;
-      sliderUl.style.transform = `translateX(${topSliderTranslateX}px)`;
+      ScrollRight(leftSibling);
+
+      // topSliderTranslateX -= 290;
+      // sliderUl.style.transform = `translateX(${topSliderTranslateX}px)`;
 
       // Уход за изображение
-      if (leftSibling.previousElementSibling) {
-        leftSibling.previousElementSibling.style.transform = `translate(${290}px, ${-20}px)`;
-        leftSibling.previousElementSibling.style.zIndex = `-1`;
-        setTimeout(function () {
-          leftSibling.previousElementSibling.style.opacity = `0`;
-        }, 300);
-        setTimeout(function () {
-          leftSibling.previousElementSibling.style.transform = `translateX(${0}px)`;
-        }, 400);
-        setTimeout(function () {
-          leftSibling.previousElementSibling.style.zIndex = `0`;
-          leftSibling.previousElementSibling.style.opacity = `1`;
-        }, 1000);
-      }
+      // if (leftSibling.previousElementSibling) {
+      //   leftSibling.previousElementSibling.style.transform = `translate(${290}px, ${-20}px)`;
+      //   leftSibling.previousElementSibling.style.zIndex = `-1`;
+      //   setTimeout(function () {
+      //     leftSibling.previousElementSibling.style.opacity = `0`;
+      //   }, 300);
+      //   setTimeout(function () {
+      //     leftSibling.previousElementSibling.style.transform = `translateX(${0}px)`;
+      //   }, 400);
+      //   setTimeout(function () {
+      //     leftSibling.previousElementSibling.style.zIndex = `0`;
+      //     leftSibling.previousElementSibling.style.opacity = `1`;
+      //   }, 1000);
+      // }
     }
   } else {
     leftSibling.classList.remove("selected");
     image.closest("li").classList.add("selected");
 
-    topSliderTranslateX -= 290;
-    sliderUl.style.transform = `translateX(${topSliderTranslateX}px)`;
+    ScrollRight(leftSibling);
 
-    // Уход за изображение
-    if (leftSibling.previousElementSibling) {
-      leftSibling.previousElementSibling.style.transform = `translate(${290}px, ${-20}px)`;
-      leftSibling.previousElementSibling.style.zIndex = `-1`;
-      setTimeout(function () {
-        leftSibling.previousElementSibling.style.opacity = `0`;
-      }, 300);
-      setTimeout(function () {
-        leftSibling.previousElementSibling.style.transform = `translateX(${0}px)`;
-      }, 400);
-      setTimeout(function () {
-        leftSibling.previousElementSibling.style.zIndex = `0`;
-        leftSibling.previousElementSibling.style.opacity = `1`;
-      }, 800);
-    }
+    // topSliderTranslateX -= 290;
+    // sliderUl.style.transform = `translateX(${topSliderTranslateX}px)`;
+
+    // // Уход за изображение
+    // if (leftSibling.previousElementSibling) {
+    //   leftSibling.previousElementSibling.style.transform = `translate(${290}px, ${-20}px)`;
+    //   leftSibling.previousElementSibling.style.zIndex = `-1`;
+    //   setTimeout(function () {
+    //     leftSibling.previousElementSibling.style.opacity = `0`;
+    //   }, 300);
+    //   setTimeout(function () {
+    //     leftSibling.previousElementSibling.style.transform = `translateX(${0}px)`;
+    //   }, 400);
+    //   setTimeout(function () {
+    //     leftSibling.previousElementSibling.style.zIndex = `0`;
+    //     leftSibling.previousElementSibling.style.opacity = `1`;
+    //   }, 1000);
+    // }
   }
 });
 
@@ -140,3 +146,92 @@ bottomSlider.addEventListener("click", function (event) {
     thumbs.style.transform = `translateX(${translateX}px)`;
   }
 });
+
+thumbs.addEventListener("click", function (event) {
+  if (event.target.tagName != "IMG") return;
+
+  const bottomSelected = event.target.getAttribute("src");
+  const topCurrentSelected = topSlider.querySelector(".selected");
+  const topSliderImages = topSlider.querySelectorAll("img");
+  let newTopSelected;
+
+  for (let image of topSliderImages) {
+    if (image.getAttribute("src") == bottomSelected) {
+      newTopSelected = image.closest("li");
+    }
+  }
+
+  topCurrentSelected.classList.remove("selected");
+  setTimeout(() => newTopSelected.classList.add("selected"), 900);
+
+  let lastTopSelectedPlace;
+  let newTopSelectedPlace;
+  for (let i = 0; i < topSlider.querySelectorAll("li").length; i++) {
+    // if (topSlider.querySelectorAll("li")[i].classList.contains("selected")) {
+    if (topSlider.querySelectorAll("li")[i] == newTopSelected) {
+      newTopSelectedPlace = i + 1;
+    }
+
+    if (topSlider.querySelectorAll("li")[i] == topCurrentSelected) {
+      lastTopSelectedPlace = i + 1;
+    }
+  }
+
+  const differenceOfPlace = newTopSelectedPlace - lastTopSelectedPlace;
+
+  if (differenceOfPlace > 0) {
+    for (let count = 0; count < differenceOfPlace; count++) {
+      ScrollRight(topCurrentSelected.nextElementSibling.previousElementSibling);
+    }
+    // topSliderTranslateX -= 290 * differenceOfPlace;
+    // sliderUl.style.transform = `translateX(${topSliderTranslateX}px)`;
+  } else if (differenceOfPlace < 0) {
+    for (let count = 0; count < -differenceOfPlace; count++) {
+      ScrollLeft(topCurrentSelected.previousElementSibling.nextElementSibling);
+    }
+    // topSliderTranslateX += 290 * -differenceOfPlace;
+    // sliderUl.style.transform = `translateX(${topSliderTranslateX}px)`;
+  }
+});
+
+function ScrollLeft(rightSibling) {
+  topSliderTranslateX += 290;
+  sliderUl.style.transform = `translateX(${topSliderTranslateX}px)`;
+
+  // Уход за изображение
+  if (rightSibling.nextElementSibling) {
+    rightSibling.nextElementSibling.style.transform = `translate(${-290}px, ${-20}px)`;
+    rightSibling.nextElementSibling.style.zIndex = `-1`;
+    setTimeout(function () {
+      rightSibling.nextElementSibling.style.opacity = `0`;
+    }, 300);
+    setTimeout(function () {
+      rightSibling.nextElementSibling.style.transform = `translateX(${0}px)`;
+    }, 400);
+    setTimeout(function () {
+      rightSibling.nextElementSibling.style.zIndex = `0`;
+      rightSibling.nextElementSibling.style.opacity = `1`;
+    }, 1000);
+  }
+}
+
+function ScrollRight(leftSibling) {
+  topSliderTranslateX -= 290;
+  sliderUl.style.transform = `translateX(${topSliderTranslateX}px)`;
+
+  // Уход за изображение
+  if (leftSibling.previousElementSibling) {
+    leftSibling.previousElementSibling.style.transform = `translate(${290}px, ${-20}px)`;
+    leftSibling.previousElementSibling.style.zIndex = `-1`;
+    setTimeout(function () {
+      leftSibling.previousElementSibling.style.opacity = `0`;
+    }, 300);
+    setTimeout(function () {
+      leftSibling.previousElementSibling.style.transform = `translateX(${0}px)`;
+    }, 400);
+    setTimeout(function () {
+      leftSibling.previousElementSibling.style.zIndex = `0`;
+      leftSibling.previousElementSibling.style.opacity = `1`;
+    }, 1000);
+  }
+}
