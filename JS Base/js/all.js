bdd93995e3e14424f3651573440ed868f88756fc
Stocil <vashurov02@mail.ru>
2023-08-26 18:56:06 +0300

    Mouse movement:

--- JS Base/js/all.js
+++ JS Base/js/all.js
@@ -1271,4 +1271,63 @@
 //   }
 // });
 
-// 19)  -------------------------------------------------------------
+// 19) Выделение текста по клику -------------------------------------------------------------
+
+// const ul = document.body.querySelector("#ul");
+
+// ul.addEventListener("click", function (event) {
+//   if (event.target.tagName != "LI") return;
+
+//   if (ul.querySelector(".selected") && event.ctrlKey == false) {
+//     ul.querySelectorAll(".selected").forEach((element) =>
+//       element.classList.remove("selected")
+//     );
+//   }
+//   event.target.classList.add("selected");
+// });
+
+// ul.addEventListener("mousedown", function (event) {
+//   event.preventDefault();
+// });
+
+// 20) Улучшенная подсказка ------------------------------------------------------------------
+
+// let tooltip;
+
+// document.onmouseover = function (event) {
+//   let anchorElem = event.target.closest("[data-tooltip]");
+
+//   if (!anchorElem) return;
+
+//   tooltip = showTooltip(anchorElem, anchorElem.dataset.tooltip);
+// };
+
+// document.onmouseout = function () {
+//   if (tooltip) {
+//     tooltip.remove();
+//     tooltip = false;
+//   }
+// };
+
+// function showTooltip(anchorElem, html) {
+//   let tooltipElem = document.createElement("div");
+//   tooltipElem.className = "tooltip";
+//   tooltipElem.innerHTML = html;
+//   document.body.append(tooltipElem);
+
+//   let coords = anchorElem.getBoundingClientRect();
+
+//   let left =
+//     coords.left + (anchorElem.offsetWidth - tooltipElem.offsetWidth) / 2;
+//   if (left < 0) left = 0;
+
+//   let top = coords.top - tooltipElem.offsetHeight - 5;
+//   if (top < 0) {
+//     top = coords.top + anchorElem.offsetHeight + 5;
+//   }
+
+//   tooltipElem.style.left = left + "px";
+//   tooltipElem.style.top = top + "px";
+
+//   return tooltipElem;
+// }
