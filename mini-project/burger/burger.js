a0c112f13c05640ea237ca9991c07acfb907d2af
Stocil <vashurov02@mail.ru>
2023-09-07 22:10:52 +0300

    burger take 2

--- mini-project/burger/burger.js
+++ mini-project/burger/burger.js
@@ -1,6 +1,42 @@
 window.addEventListener("resize", function (event) {
-  // console.log(document.documentElement.clientWidth);
-  if (document.documentElement.clientWidth < 1000) {
-    console.log(document.documentElement.clientWidth);
+  if (document.documentElement.clientWidth <= 1000) {
+    const descContent = this.document.body.querySelector(
+      ".header__content-desc"
+    );
+    const mobileContent = this.document.body.querySelector(
+      ".header__content-mobile"
+    );
+
+    descContent.style.display = "none";
+    mobileContent.style.display = "flex";
   }
+});
+
+window.addEventListener("resize", function (event) {
+  if (document.documentElement.clientWidth > 1000) {
+    const descContent = this.document.body.querySelector(
+      ".header__content-desc"
+    );
+    const mobileContent = this.document.body.querySelector(
+      ".header__content-mobile"
+    );
+
+    descContent.style.display = "flex";
+    mobileContent.style.display = "none";
+  }
+});
+
+const burgerButton = document.body.querySelector(".header__burger");
+const burgerInner = document.body.querySelector(".burger__inner");
+
+burgerButton.addEventListener("click", function (event) {
+  if (!event.target.closest(".header__burger")) return;
+
+  burgerButton.classList.toggle("header__burger__active");
+
+  burgerButton.firstElementChild.classList.toggle("burger__first-line__active");
+  burgerButton.children[1].classList.toggle("burger__second-line__active");
+  burgerButton.lastElementChild.classList.toggle("burger__last-line__active");
+
+  burgerInner.classList.toggle("active");
 });
