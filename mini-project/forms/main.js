cc78ddd61e5f1f380e304c79369d07837ace8b67
Stocil <vashurov02@mail.ru>
2023-09-16 22:55:46 +0300

    form checkboxes styled

--- mini-project/forms/main.js
+++
@@ -1,4 +0,0 @@
-const a = 10;
-const b = 110;
-const message = `${a} + ${b} = ${a + b}`;
-console.log(message);
--- mini-project/forms/main.ts
+++ mini-project/forms/main.ts
@@ -1,6 +1,1 @@
-const a: number = 10;
-const b: number = 110;
 
-const message: string = `${a} + ${b} = ${a + b}`;
-
-console.log(message);
