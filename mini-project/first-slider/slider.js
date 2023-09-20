048c78fb11f1638cfca7c52805926594f2509bc5
Stocil <vashurov02@mail.ru>
2023-09-20 19:39:15 +0300

    slider take 2

--- mini-project/first-slider/slider.js
+++ mini-project/first-slider/slider.js
@@ -1,13 +1,48 @@
 const body = document.body;
+const slider = body.querySelector(".slider");
 const sliderItems = body.querySelectorAll(".slider__list__item");
 const sliderItemsCount = sliderItems.length;
 const sliderItemWidth = 250;
 const sliderList = body.querySelector(".slider__list");
 const sliderListGap = +getComputedStyle(sliderList).gap.slice(0, -2);
 const sliderListGapCount = sliderItemsCount - 1;
-const sliderWidth = Math.round(sliderItemsCount * sliderItemWidth + sliderListGap * sliderListGapCount) + "px";
-sliderList.style.width = sliderWidth;
+const sliderWidth = Math.round(sliderItemsCount * sliderItemWidth + sliderListGap * sliderListGapCount);
+sliderList.style.width = sliderWidth + "px";
 for (let index = 0; index < sliderItemsCount; index++) {
     sliderItems[index].setAttribute("aria-label", `${index + 1} / ${sliderItemsCount}`);
     sliderItems[index].setAttribute("data-slide-index", `${index}`);
+}
+// Slider buttons
+let sliderTranslate = 0;
+let slideIndex = 3;
+const maxSlideIndex = sliderItemsCount - 1;
+slider.addEventListener("click", scrollSlider);
+function scrollSlider(event) {
+    const target = event.target;
+    if (!target.closest(".slider__button"))
+        return;
+    if (target.closest(".slider__button").classList.contains("slider__left-button")) {
+        if (sliderIsOver("left"))
+            return;
+        sliderTranslate += 260;
+        slideIndex--;
+    }
+    else {
+        if (sliderIsOver("right"))
+            return;
+        sliderTranslate -= 260;
+        slideIndex++;
+    }
+    sliderList.style.transform = `translateX(${sliderTranslate}px)`;
+}
+function sliderIsOver(side) {
+    if (side === "left") {
+        if (sliderTranslate >= 0)
+            return true;
+    }
+    if (side === "right") {
+        if (-sliderTranslate + 250 >= sliderWidth - slider.offsetWidth)
+            return true;
+    }
+    return false;
 }
--- mini-project/first-slider/slider.ts
+++ mini-project/first-slider/slider.ts
@@ -1,4 +1,5 @@
 const body = document.body;
+const slider: HTMLElement = body.querySelector(".slider");
 
 const sliderItems: NodeListOf<HTMLElement> = body.querySelectorAll(
   ".slider__list__item"
@@ -10,12 +11,11 @@ const sliderList: HTMLElement = body.querySelector(".slider__list");
 const sliderListGap = +getComputedStyle(sliderList).gap.slice(0, -2);
 const sliderListGapCount = sliderItemsCount - 1;
 
-const sliderWidth =
-  Math.round(
-    sliderItemsCount * sliderItemWidth + sliderListGap * sliderListGapCount
-  ) + "px";
+const sliderWidth = Math.round(
+  sliderItemsCount * sliderItemWidth + sliderListGap * sliderListGapCount
+);
 
-sliderList.style.width = sliderWidth;
+sliderList.style.width = sliderWidth + "px";
 
 for (let index = 0; index < sliderItemsCount; index++) {
   sliderItems[index].setAttribute(
@@ -24,4 +24,45 @@ for (let index = 0; index < sliderItemsCount; index++) {
   );
 
   sliderItems[index].setAttribute("data-slide-index", `${index}`);
+}
+
+// Slider buttons
+let sliderTranslate = 0;
+let slideIndex = 3;
+const maxSlideIndex = sliderItemsCount - 1;
+
+slider.addEventListener("click", scrollSlider);
+
+function scrollSlider(event: MouseEvent) {
+  const target = event.target as HTMLElement;
+
+  if (!target.closest(".slider__button")) return;
+
+  if (
+    target.closest(".slider__button").classList.contains("slider__left-button")
+  ) {
+    if (sliderIsOver("left")) return;
+
+    sliderTranslate += 260;
+    slideIndex--;
+  } else {
+    if (sliderIsOver("right")) return;
+
+    sliderTranslate -= 260;
+    slideIndex++;
+  }
+
+  sliderList.style.transform = `translateX(${sliderTranslate}px)`;
+}
+
+function sliderIsOver(side: string): boolean {
+  if (side === "left") {
+    if (sliderTranslate >= 0) return true;
+  }
+
+  if (side === "right") {
+    if (-sliderTranslate + 250 >= sliderWidth - slider.offsetWidth) return true;
+  }
+
+  return false;
 }
