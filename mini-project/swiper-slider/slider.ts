f3c7bff30ab5a56477d9f797e0cb9629890a73a5
Stocil <vashurov02@mail.ru>
2023-09-22 15:41:20 +0300

    slider on swiper-slider complite

---
+++ mini-project/swiper-slider/slider.ts
@@ -0,0 +1,25 @@
+new Swiper(".swiper", {
+  // Optional parameters
+  loop: true,
+
+  pagination: {
+    clickable: true,
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  keyboard: {
+    enabled: true,
+  },
+
+  mousewheel: {
+    sensitivity: 1,
+  },
+
+  speed: 1000,
+});
